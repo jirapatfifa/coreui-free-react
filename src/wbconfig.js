55d0cb9b74f968b5a4518380ac379f79a2538f40
Jirapat Tanuwoharn <jirapat_ta66@live.rmutl.ac.th>
2024-10-21 13:48:39 +0700

    config

--- src/wbconfig.js
+++ src/wbconfig.js
@@ -1,15 +1,15 @@
 const wbconfig = {
     development: {
-        hosturl  : 'https://lab-parse-server.se-rmutl.net/api',
-        wsurl    : 'wss://lab-parse-server.se-rmutl.net',
+        hosturl  : 'https://localhost:8705/api',
+        wsurl    : 'wss://https://localhost:8705',
         masterKey     : 'wallboardapi',
         clientKey     : 'wallboardapi',
         javascriptKey : 'wallboardapi',
         appId : "wallboardapi"
     },
     production: {
-        hosturl  : 'https://localhost:8505/api',
-        wsurl    : 'wss://localhost:8505',
+        hosturl  : 'https://localhost:8705/api',
+        wsurl    : 'wss://localhost:8705',
         masterKey     : 'wallboardapi',
         clientKey     : 'wallboardapi',
         javascriptKey : 'wallboardapi',
